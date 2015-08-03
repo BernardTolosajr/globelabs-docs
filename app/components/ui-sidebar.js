f4c3ba97e05245ec4d8d59090cef3d0429c786ab
BT <bernardotolosajr@gmail.com>
2015-08-03 10:10:35 +0800

    remove unused content

--- app/components/ui-sidebar.js
+++ app/components/ui-sidebar.js
@@ -3,16 +3,6 @@ import $ from 'jquery';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    $('.masthead').visibility({
-          once: false,
-          onBottomPassed: function() {
-            $('.fixed.menu').transition('fade in');
-          },
-          onBottomPassedReverse: function() {
-            $('.fixed.menu').transition('fade out');
-          }
-        });
-
     $('.ui.sidebar').sidebar('attach events', '.toc.item');
   }
 });
